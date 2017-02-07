b72478b20767aae2a33160e4250dd1e0ed0de409
Varun Singh <varun.singh2729@gmail.com>
2017-02-07 15:24:51 +0530

    Draft 2: Optimized and fixes

--- headwig.sh
+++ headwig.sh
@@ -1,12 +1,15 @@
 #! /bin/bash
 
-# filename has list of urls from command line
+# filename has list of multiple urls
 # filename = "$1"
 
 echo "Enter URL: "
 read url
+
+# loop for reading multiple urls from file. Ignore otherwise
 # while read -r url || [ -n "$url" ]
 # do
+
     # Set mode to get content of page in HTML
     url="${url}?action=render"
 
@@ -21,69 +24,88 @@ read url
     # remove all empty lines
     sed -i '/^$/d' scraped.txt
 
-    # split file in lines with files named chunk.0001 chunk.0002 ..
-    split --lines=1 --numeric-suffixes --suffix-length=4 scraped.txt chunk.
+    # Find patterns and modify them one at a time
+    grep -o '\(\. \)\?\([^\.]\|[0-9]*\.[0-9]\+\)*\(\[[0-9]\+]\)\+[^\.]*' scraped.txt > inter.txt
 
-    # Run regex on all files and combine results
-    IN_FILES=`find . -type f -name "chunk*" | sort`
-    for file in $IN_FILES
+    # If citations is present then proceed
+    len=1
+    while grep -q '\[[0-9]\+]' inter.txt
     do
-        # Find patterns and modify them one at a time
-        grep -o '\(\. \)\?\([^\.]\|[0-9]*\.[0-9]\+\)*\(\[[0-9]\+]\)\+[^\.]*' $file >> inter.txt
+        # select top most line with citations
+        str=`grep -o '\. \([^\.]\|[0-9]*\.[0-9]\+\)*\(\[[0-9]\+]\)\+[^\.]*' inter.txt | head -1`
 
-        # If citations is present then proceed
-        if grep -q '\[[0-9]\+]' inter.txt;
-            then
+        # Store all citation numbers in a var after removing []
+        cite=`echo $str | grep -o '\[[0-9]\+]' | sed 's/\(\[\|]\)//g'`
 
+        #Remove only first occurence of '. ' if present in beg
+        sed -i -e '0,/\. / s///' inter.txt
+        sed -i 's/\. //g' inter.txt
 
-                # Store all citation numbers in a var after removing []
-                cite=`grep -o '\[[0-9]\+]' inter.txt |  sed 's/\(\[\|]\)//g'`
+        # Remove cites from first line of main text
+        sed -i $len,$len's/\[[0-9]\+]//g' inter.txt
 
-                #Remove '. ' if present in beg
-                sed -i 's/\. //g' inter.txt
+        # Append ~ around numbers around nth line and create a result file
+        ptext=`cat inter.txt | sed "${len}q;d"`
+        ptext="~ $cite ~ $ptext"
 
-                # Remove matches from main text
-                sed -i 's/\[[0-9]\+]//g' inter.txt
+        echo $ptext >> lines.txt
+        ((len++))
+    done
 
-                # Append ~ around numbers and create a result file
-                ptext=`cat inter.txt`
-                ptext="~ $cite ~ $ptext"
-                echo $ptext >> lines.txt
+    while true
+    do
+
+        # Take queries
+        echo "Chose type of query"
+        echo "[1]: Get lines with citation X"
+        echo "[2]: Get citations of a line"
+
+        read choice
+
+        case "$choice" in
+            #   For queries of type 1
+            1)  echo "Enter citation number: "
+                read inp
+
+                # Use cite shell variable in regex to search from result set
+                res=`cat lines.txt | grep ".* $inp .*"`
+
+                # Remove all citation numbers from result
+                res=`echo $res | sed 's/~.\+~//g'`
+
+                # Print output
+                echo $res
+                ;;
+
+            #   For queries of type 2
+            2)  echo "Enter string: "
+                read str
+
+                # select searched line from result file
+                res=`cat lines.txt | grep -o "$str"`
+
+                # remove text and keep citation numbers
+                res=`echo $res | grep -o '~.~'`
+
+                # Print output
+                echo $res
+                ;;
+
+            #   Default case
+            *)  echo "Invalid Input"
+                echo "The program will now exit..."
+                ;;
+
+        esac
+
+        read "Do you wish to continue? (y/n)" wish
+        echo $wish
+
+        if [ "$wish" == "y" ]; then
+            break
         fi
     done
 
-    # cleanup directory
-    rm chunk.*
-
-    # Take queries
-    echo "Chose type of query"
-    echo "[1]: Get lines with citation X"
-    echo "[2]: Get citations of a line"
-    read choice
-
-    case "$choice" in
-        #   For queries of type 1
-        1)  echo "Enter citation number: "
-            echo $cite
-
-            # Use cite shell variable in regex to search from result set
-            cat lines.txt | grep ".* $cite .*" > final.txt
-
-            # Remove all citation numbers from result
-            sed -i 's/~.\+~//g' final.txt
-
-            cat final.txt
-            ;;
-
-        #   For queries of type 2
-        2)  echo "Enter string: "
-            read str
-
-            cat lines.txt | grep -o "$str" > final.txt
-
-            cat final.txt | grep -o '~.~'
-            ;;
-    esac
 # done < "$filename"
 
 
