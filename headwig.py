b3da32d794f8b48df9ff72b0330b46d2a73935ae
Varun Singh <varun.singh2729@gmail.com>
2017-02-12 04:21:36 +0530

    Added Tests and fixes

--- headwig.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-with open('urls.txt') as inp:
-    for line in inp:
-        print(line)
-        source = requests.get(line)
-        soup = BeautifulSoup(source.content,'html.parser')
-        text = soup.get_text()
-        print (text)
